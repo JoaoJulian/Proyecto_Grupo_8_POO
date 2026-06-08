dd121e7e3034f8a42522231593f09e3dc488b0f7
JoaoJulian <joao.julian.m@uni.pe>
2026-06-08 18:04:31 -0500

    Add files via upload

---
+++ presupuesto-personal/src/main/java/com/presupuesto/presupuesto_personal/PresupuestoPersonalApplication.java
@@ -0,0 +1,13 @@
+package com.presupuesto.presupuesto_personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresupuestoPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresupuestoPersonalApplication.class, args);
+	}
+
+}
---
+++ presupuesto-personal/src/test/java/com/presupuesto/presupuesto_personal/PresupuestoPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.presupuesto.presupuesto_personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresupuestoPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
